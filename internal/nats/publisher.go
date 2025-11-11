c26fcd993d19a4bdf21e39a89ef8f4f31f15069f
mahdi <m.m.fallah118@gmail.com>
2025-11-11 17:29:03 +0330

    fix bug

--- internal/nats/publisher.go
+++ internal/nats/publisher.go
@@ -1,18 +1,37 @@
 package nats
 
 import (
+	"encoding/json"
 	"fmt"
 	"go-auth-service/pkg/logger"
 
 	"github.com/nats-io/nats.go"
 )
 
-func PublishUserRegistered(js nats.JetStreamContext, userID string) error {
+type UserRegisteredEvent struct {
+	UserID string `json:"user_id"`
+	Email  string `json:"email"`
+	Name   string `json:"name,omitempty"`
+}
+
+func PublishUserRegistered(js nats.JetStreamContext, userID string, email string, name string) error {
 	if js == nil {
 		return fmt.Errorf("JetStream context is nil")
 	}
 
-	_, err := js.Publish("user.registered", []byte(userID))
+	event := UserRegisteredEvent{
+		UserID: userID,
+		Email:  email,
+		Name:   name,
+	}
+
+	payload, err := json.Marshal(event)
+	if err != nil {
+		logger.Error("Failed to marshal user.registered event: " + err.Error())
+		return fmt.Errorf("failed to marshal event: %v", err)
+	}
+
+	_, err = js.Publish("user.registered", payload)
 	if err != nil {
 		logger.Error("Failed to publish user.registered: " + err.Error())
 		return fmt.Errorf("failed to publish: %v", err)
